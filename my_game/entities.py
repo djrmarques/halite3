b1cff81a2ff0bc2bf3012bcc20edc648c3627203
davi <davidmarques856@gmail.com>
2018-12-29 19:53:03 +0000

    Started writing some entities

--- my_game/entities.py
+++ my_game/entities.py
@@ -7,6 +7,17 @@ class Ship:
         ship.halite_amount = 0
 
     def move(self, direction):
-        ''' Move into a certain direction '''
-        pass
+        ''' 
+        Move into a certain direction 
+        direction is a tuple like (0, -1)
+        '''
+        ship.x += direction[0]
+        ship.y += direction[1]
 
+    def collect_halite(self):
+        ''' Collects halite from the field '''
+        self.halite += 0.25*field.loc[ship.x, ship.y]
+        field.loc[ship.x, ship.y] *= 0.75
+
+        if self.halite > 1000:
+            self.halite = 1000
