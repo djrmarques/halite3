b19a64885e279ec1a9259d07b40749c183e752e0
davi <davidmarques856@gmail.com>
2018-12-31 11:45:30 +0000

    Cleaned up the testing zone

--- my_game/simulation.py
+++ my_game/simulation.py
@@ -1,22 +1,11 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.sparse.csgraph import dijkstra
 
 # Read Map
-df = pd.read_csv("map.csv", header=None).transpose()
+df = pd.read_csv("seed1.csv", header=None).transpose()
 
-d = np.multiply(df.values.copy(), 0.1)
-d = d[:5, :5]
+start_coord = (16, 16)
 
-def best_route(start, end):
-    ''' Determines the best route '''
-    spat
-
-start = (4, 4)
-end = (0, 0)
-
-dij = dijkstra(d, indices=(start, end))
-
-plt.matshow(d)
+plt.matshow(df)
 plt.show()
